96f5927739d0018064642eb89557cfb2c96cb21b
Sydney Ramos <sydneyramos2028@gmail.com>
2025-07-11 10:10:52 -0400

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Sofia Ramos on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
